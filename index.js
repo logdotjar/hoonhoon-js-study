cf4d0de1d9383372ee801cd9db1b32be52dee5b6
jinareum <jinareum.76@gmail.com>
2023-01-09 22:34:35 +0900

    🔍섹션5. 함수활용 및 localStorage 기록 정리 및 JS 코드 수정

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const messageContainer = document.querySelector("#d-day-message");
 const container = document.querySelector("#d-deay-container");
 const intervalIdArr = [];
 
+//로컬 스토리지에서 데이터 다시 가져오기 - key
+const savedDate = localStorage.getItem('saved-data');
+
 messageContainer.innerHTML = '<h3>D-day를 입력해주세요.</h3>';
+
+
 
 
 const dateFormMaker = function () {
@@ -14,10 +19,15 @@ const dateFormMaker = function () {
     return dateFormat;
     // console.log(inputYear , inputMonth , inputDate);
 }
-const countMaker = function () {
+const countMaker = function (data) {
+    if(data !== savedDate){
+        //새로고침해도 카운트가 유지될 수 있도록 비휘발성 메모리 local storage에 저장
+        //입력되어있는 데이터와 새로운 데이터 다르다면 새로운 데이터를 저장한다.
+        localStorage.setItem('saved-date',data);
+    }
 
     const nowDate = new Date();
-    const targetDate = new Date('2023-12-25');
+    const targetDate = new Date(data).setHours(0,0,0,0);
     const remaining = (targetDate - nowDate) / 1000;
     //remaining이 0이라면, 타이머가 종료되었습니다 출력
     if(remaining <= 0){
@@ -68,9 +78,20 @@ const countMaker = function () {
     // const docKeys = Object.keys(documentObj);
 
 
+    const format = function (time) {
+        if(time<10){
+            // 한자리 숫자를 두자리로 동일하게 표기
+            return "0" + time;
+        } else{
+            // 두자리 수일때 그대로 반환
+            return time;
+        }
+    }
+
     let i = 0;
     for(let tag of documentArr){
-        document.getElementById(tag).textContent = remainingObj[timeKeys[i]];
+        const remainingTime = format(remainingObj[timeKeys[i]]);
+        document.getElementById(tag).textContent = remainingTime;
         i++;
     }
 
@@ -109,12 +130,26 @@ const countMaker = function () {
 };
 
 
-const starter = function(){
+const starter = function(targetDateInput){
+
+    // falsy한 값 undefined의 부정
+    if(!targetDateInput){
+        //매개변수 재할당 가능
+        targetDateInput = dateFormMaker();
+    }
+
+    // const targetDateInput = dateFormMaker();
+
+
     container.style.display = "flex";
     messageContainer.style.display = "none";
+    setClearInterval(); // 기존에 존재하던 데이터 clear 후 새롭게 D-day 카운트
     //1초 기다리기전에 먼저 함수 실행
-    countMaker();
-    const intervalId = setInterval(countMaker,1000);
+    countMaker(targetDateInput);
+    //함수내부에서 실행해주고자 할때 setinterval내에서 함수실행
+    const intervalId = setInterval(()=> {
+        countMaker(targetDateInput)
+    },1000);
     // for(let i = 0; i <100; i++){
     //     setTimeout(countMaker,1000 * i);
     // }
@@ -122,11 +157,24 @@ const starter = function(){
 }
 
 const setClearInterval = function (){
-    container.style.display = "none";
-    messageContainer.innerHTML = "<h3>D-day를 입력해주세요.</h3>"
-    messageContainer.style.display = "none";
+    localStorage.removeItem('saved-date');
     for(let i = 0; i < intervalIdArr.length; i++){
         clearInterval(intervalIdArr[i])
     }
+}
 
+const resetTimer = function () {
+    container.style.display = "none";
+    messageContainer.innerHTML = "<h3>D-day를 입력해주세요.</h3>"
+    messageContainer.style.display = "none";
+    setClearInterval();
+}
+
+// storage에 저장되어있는지 체크
+// 조건 truthy하기 떄문에 조건식 성립
+if (savedDate){
+    starter(savedDate);
+} else {
+    container.style.display = "none";
+    messageContainer.innerHTML = "<h3>D-day를 입력해주세요.</h3>"
 }
